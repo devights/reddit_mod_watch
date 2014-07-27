134ad2c09c303793a6d68ab7ac73809915a6e2c7
Stephen De Vight <devights@gmail.com>
2014-07-27 15:25:30 -0700

    test importer

--- data_import/management/commands/test.py
+++ data_import/management/commands/test.py
@@ -11,22 +11,39 @@ class Command(BaseCommand):
         parser.add_argument('poll_id', nargs='+', type=int)
 
     def handle(self, *args, **options):
+        store_moderators_for_subreddit("holocaust")
+
+        # user = User.objects.get(username='sodypop')
+        # get_modded_subs_by_user(user)
+        sub_start = datetime.now()
         subs = Subreddit.objects.filter(last_updated=None)
         for sub in subs:
             start = datetime.now()
-            store_moderators_for_subreddit(sub.name)
+            try:
+                store_moderators_for_subreddit(sub.name)
+            except Exception as ex:
+                print ex
             time_taken = (datetime.now() - start).seconds
             if time_taken < 2:
                 sleep(2-time_taken)
-
+        sub_time = (datetime.now() - sub_start).seconds
+        user_start = datetime.now()
         users = User.objects.filter(last_updated=None)
         for user in users:
+            if user.username == "AutoModerator":
+                continue
             start = datetime.now()
-            get_modded_subs_by_user(user)
+            try:
+                get_modded_subs_by_user(user)
+            except Exception as ex:
+                print ex
             time_taken = (datetime.now() - start).seconds
             if time_taken < 2:
                 sleep(2-time_taken)
+        user_time = (datetime.now() - user_start).seconds
 
+        print "Added %s subs in %s seconds \nand %s users in %s seconds)"\
+              % (len(subs), sub_time, len(users), user_time)
 
 
 
